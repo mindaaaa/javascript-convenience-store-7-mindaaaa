9f03896336e3e75f0ca4baef722e99cc13d15a82
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-07 19:35:47 +0900

    feat(Inventory): 클래스 초기 생성, 값 객체 및 기본 유효성 검사 구현

--- src/domain/Inventory.js
+++ src/domain/Inventory.js
@@ -1,1 +1,43 @@
-class Inventory {}
+class Inventory {
+  hasPromotion;
+  #products;
+
+  constructor(products) {
+    this.#validate(products);
+    this.#products = products;
+  }
+
+  #validate(product) {
+    const { name, price, quantity, promotion } = product;
+
+    this.#validateName(name);
+    this.#validatePrice(price);
+    this.#validateQuantity(quantity);
+    this.#validatePromotion(promotion);
+  }
+
+  #validateName(name) {
+    if (!name) {
+      throw new Error('[ERROR] 상품명은 필수입니다.');
+    }
+  }
+
+  #validatePrice(price) {
+    if (price == null || price <= 0 || !Number.isInteger(price)) {
+      throw new Error('[ERROR] 가격은 양의 정수여야합니다.');
+    }
+  }
+
+  #validateQuantity(quantity) {
+    if (!Number.isInteger(quantity) || quantity < 0) {
+      throw new Error('[ERROR] 재고 수량은 null값과 양의 정수만 허용됩니다.');
+    }
+  }
+
+  validPromotions = ['1+1', '2+1', 'MD추천상품', '반짝할인'];
+  #validatePromotion(promotion) {
+    if (promotion && !this.validPromotions.includes(promotion)) {
+      throw new Error(`[ERROR] 유효하지 않은 프로모션: ${promotion}`);
+    }
+  }
+}
