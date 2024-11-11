26acb1d3a55fe97b79a02888ec226bd3c298d958
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-11 11:14:55 +0900

    feat(Checkout): 사용자 입력으로만 구매 계획 초안을 생성하는 CheckOut 클래스 구현

--- src/services/Checkout.js
+++ src/services/Checkout.js
@@ -1,1 +1,25 @@
-class Checkout {}
+class Checkout {
+  #shelves;
+
+  constructor(shelves) {
+    this.#shelves = shelves;
+  }
+
+  createPaymentPlan(shoppingCart) {
+    return shoppingCart.goods.map(({ name, quantity }) => {
+      return this.createPaymentSummary(name, quantity);
+    });
+  }
+
+  createPaymentSummary(name, quantity) {
+    const targetGoods = this.#shelves.goods.find(
+      (goods) => goods.name === name
+    );
+
+    if (!targetGoods) {
+      throw new Error('[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.');
+    }
+
+    return targetGoods.getPaymentSummary(quantity);
+  }
+}
