9fcaf7cc56af9bbdce1280caa44723dfedfa8ac9
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-11 17:15:54 +0900

    refactor(Cashier): Cashier 클래스 기능별 메서드 분리
    - processPlan: 개별 정보를 처리해 결제 계획 생성
    - processFreebie: 증정품의 객체를 반환함
    - calculateTotalPrice: 배열의 총 가격 계산
    - calculateTotalCount: 배열의 총 수량 계산
    - calculateDiscountByMembership: 멤버십 할인 여부와 최종 할인 금액 계산
    - calculateDiscountAmount: 증정품이 없는 상품의 멤버십 할인 금액 계산

--- src/domain/Cashier.js
+++ src/domain/Cashier.js
@@ -10,34 +10,9 @@ class Cashier {
   }
 
   checkout(confirmedPlans, shouldDiscount) {
-    // [{ name: '콜라', price: 1000, requestedQuantity: 6, freebieCount: 2, quantity: { regular: 0, promotional: 6 } }]
-    const results = [];
-
-    let sumOfPrice = 0;
-    let goodsCount = 0;
-    for (const { name, price, freebieCount, quantity } of confirmedPlans) {
-      const currentCount = quantity.regular + quantity.promotional;
-      goodsCount += currentCount;
-
-      const currentPrice = currentCount * price;
-      sumOfPrice += currentPrice;
-
-      const result = {
-        name,
-        count: currentCount,
-        price: currentPrice,
-        freebie: null,
-      };
-      if (freebieCount > 0) {
-        result.freebie = {
-          count: freebieCount,
-          price: freebieCount * price,
-        };
-      }
-
-      results.push(result);
-    }
-
+    const results = confirmedPlans.map((plan) => this.#processPlan(plan));
+    const sumOfPrice = this.#calculateTotalPrice(results);
+    const goodsCount = this.#calculateTotalCount(results);
     const discountByMembership = this.#calculateDiscountByMembership(
       results,
       shouldDiscount
@@ -46,22 +21,49 @@ class Cashier {
     return new Receipt(sumOfPrice, goodsCount, results, discountByMembership);
   }
 
-  #calculateDiscountByMembership(results, shouldDiscount) {
-    if (!shouldDiscount) {
-      return 0;
-    }
+  #processPlan({ name, price, freebieCount, quantity }) {
+    const currentCount = quantity.regular + quantity.promotional;
+    const currentPrice = currentCount * price;
 
-    const discount =
+    const result = {
+      name,
+      count: currentCount,
+      price: currentPrice,
+      freebie: this.#processFreebie(freebieCount, price),
+    };
+
+    return result;
+  }
+
+  #processFreebie(freebieCount, price) {
+    if (freebieCount > 0) {
+      return { count: freebieCount, price: freebieCount * price };
+    }
+    return null;
+  }
+
+  #calculateTotalPrice(results) {
+    return results.reduce((total, { price }) => total + price, 0);
+  }
+
+  #calculateTotalCount(results) {
+    return results.reduce((acc, { count }) => acc + count, 0);
+  }
+
+  #calculateDiscountByMembership(results, shouldDiscount) {
+    if (!shouldDiscount) return 0;
+
+    const discount = this.#calculateDiscountAmount(results);
+    return Math.min(discount, this.#maximumDiscountAmount);
+  }
+
+  #calculateDiscountAmount(results) {
+    return (
       results
         .filter((e) => !e.freebie)
         .reduce((acc, target) => acc + target.price, 0) *
-      this.#discountPercentage;
-
-    if (discount > this.#maximumDiscountAmount) {
-      return this.#maximumDiscountAmount;
-    }
-
-    return discount;
+      this.#discountPercentage
+    );
   }
 }
 
