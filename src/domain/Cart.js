f66e6db1ed4c2e9e2745633147883790466aa816
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-11 10:57:55 +0900

    feat(Cart): 사용자가 구매를 원하는 상품(예: [콜라-3],[사이다-2])을 검증하고 객체로 parse하는 Cart 클래스 구현

---
+++ src/domain/Cart.js
@@ -0,0 +1,46 @@
+class Cart {
+  #items;
+
+  constructor(userInput) {
+    if (typeof userInput !== 'string' || userInput.length) {
+      throw new Error(
+        '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.'
+      );
+    }
+
+    this.#items = this.#tryParseToList(input);
+  }
+
+  #tryParseToList(userInput) {
+    const regex =
+      /^\[([가-힣a-zA-Z0-9]+)-([1-9]\d*)](,\[([가-힣a-zA-Z0-9]+)-([1-9]\d*)])*$/;
+    if (!regex.test(userInput)) {
+      throw new Error(
+        '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.'
+      );
+    }
+
+    const parsedList = userInput
+      .trim()
+      .split(',')
+      .map((e) => {
+        const [name, quantity] = e.trim().slice(1, -1).split('-');
+
+        return { name, quantity: Number(quantity) };
+      });
+
+    if (parsedList.length !== new Set(parsedList.map((e) => e.name)).size) {
+      throw new Error(
+        '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.'
+      );
+    }
+
+    return parsedList;
+  }
+
+  get goods() {
+    return this.#items;
+  }
+}
+
+export default Cart;
