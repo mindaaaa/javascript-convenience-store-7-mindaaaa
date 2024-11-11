a9134e46fa04913b59dd0f3d2ede36ec57de75d2
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2024-11-11 16:34:39 +0900

    style(Receipt): 영수증 출력 형식 맞춤 및 들여쓰기 조정

--- src/domain/Receipt.js
+++ src/domain/Receipt.js
@@ -13,13 +13,13 @@ class Receipt {
 
   toString() {
     const primarySection = this.#data.map((e) => {
-      return `${e.name}\t${e.count}\t${e.price.toLocaleString()}`;
+      return `${e.name}\t\t${e.count}\t${e.price.toLocaleString()}`;
     });
 
     const promotionalTargets = this.#data.filter((e) => e.freebie);
 
     const secondarySection = promotionalTargets.map((e) => {
-      return `${e.name}\t${e.freebie.count}`;
+      return `${e.name}\t\t${e.freebie.count}`;
     });
 
     const discountByPromotion = promotionalTargets.reduce((acc, target) => {
@@ -29,18 +29,16 @@ class Receipt {
     const result =
       this.#priceSum - discountByPromotion - this.#discountByMembership;
 
-    return `===========W 편의점=============
-      상품명\t수량\t금액
-      ${primarySection.join('\n')}
-      ===========증\t정=============
-      ${secondarySection.join('\n')}
-      ==============================
-      총구매액\t${this.#countSum}\t${this.#priceSum.toLocaleString()}
-      행사할인\t\t-${discountByPromotion.toLocaleString()}
-      멤버십할인\t\t-${this.#discountByMembership.toLocaleString()}
-      내실돈\t\t${result.toLocaleString()}
-      
-      `;
+    return `===========W 편의점============
+상품명\t\t수량\t금액
+${primarySection.join('\n')}
+===========증\t정=============
+${secondarySection.join('\n')}
+===============================
+총구매액\t${this.#countSum}\t${this.#priceSum.toLocaleString()}
+행사할인\t\t-${discountByPromotion.toLocaleString()}
+멤버십할인\t\t-${this.#discountByMembership.toLocaleString()}
+내실돈\t\t\t${result.toLocaleString()}`;
   }
 }
 
